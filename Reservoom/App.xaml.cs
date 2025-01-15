ce295c39dbc31f300f77219501e1d2628e09aa73
Sotatek-HungNguyen16 <hung.nguyen16@sotatek.com>
2025-01-15 16:00:21 +0700

    Create Models

--- Reservoom/App.xaml.cs
+++ Reservoom/App.xaml.cs
@@ -1,5 +1,11 @@
-﻿using System.Configuration;
+﻿using Reservoom.Exceptions;
+using Reservoom.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
 using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
 using System.Windows;
 
 namespace Reservoom
@@ -9,6 +15,31 @@ namespace Reservoom
     /// </summary>
     public partial class App : Application
     {
-    }
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            Hotel hotel = new Hotel("SingletonSean Suites");
 
+            try
+            {
+                hotel.MakeReservation(new Reservation(
+                    new RoomID(1, 3),
+                    "SingletonSean",
+                    new DateTime(1995, 1, 2),
+                    new DateTime(1999, 1, 3)));
+                hotel.MakeReservation(new Reservation(
+                    new RoomID(1, 3),
+                    "SingletonSean",
+                    new DateTime(2000, 1, 1),
+                    new DateTime(2000, 1, 2)));
+            }
+            catch (ReservationConflictException ex)
+            {
+
+            }
+
+            IEnumerable<Reservation> reservations = hotel.GetReservationsForUser("SingletonSean");
+
+            base.OnStartup(e);
+        }
+    }
 }
